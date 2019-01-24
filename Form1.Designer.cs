9b47b4f07fe214498f0925df2ae16fe30dfc86fc
wuxu <WX15665828994@163.com>
2019-01-24 15:45:10 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace MeVido
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 507);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(301, 512);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "下一帧";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(571, 512);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "停止";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(7, 462);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(955, 45);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(714, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "视频宽度：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(714, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "视频名字： ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(714, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "视频高度：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(714, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "视频帧数：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(714, 207);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "当前帧数：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(714, 247);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 12);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "x：";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(788, 246);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 12);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "y:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(716, 275);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 12);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "R:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(788, 275);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(17, 12);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "G:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(843, 274);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(17, 12);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "B:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(786, 207);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(11, 12);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "0";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(743, 247);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(11, 12);
+            this.label12.TabIndex = 18;
+            this.label12.Text = "0";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(811, 247);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(11, 12);
+            this.label13.TabIndex = 19;
+            this.label13.Text = "0";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(743, 274);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(11, 12);
+            this.label14.TabIndex = 20;
+            this.label14.Text = "0";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(811, 275);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(11, 12);
+            this.label15.TabIndex = 21;
+            this.label15.Text = "0";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(866, 275);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(11, 12);
+            this.label16.TabIndex = 22;
+            this.label16.Text = "0";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(788, 57);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(47, 12);
+            this.label17.TabIndex = 23;
+            this.label17.Text = "label17";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(786, 95);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(11, 12);
+            this.label18.TabIndex = 24;
+            this.label18.Text = "0";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(786, 135);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(11, 12);
+            this.label19.TabIndex = 25;
+            this.label19.Text = "0";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(786, 171);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(11, 12);
+            this.label20.TabIndex = 26;
+            this.label20.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 547);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,334 @@
+﻿
+
+
+using DirectShowLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MeVido
+{
+    public partial class Form1 : Form
+    {
+        long time1;
+        int i = 0;
+        const int WMGraphNotify = 0x0400 + 13;
+        string file;
+        Guid TIME_FORMAT_FRAME = new Guid("7b785570-8c82-11cf-bc0c-00aa00ac74f6");
+        Thread t = null;
+        const int WM_APP = 0x8000;
+        const int WM_GRAPHNOTIFY = WM_APP + 1;
+        const int EC_COMPLETE = 0x01;
+        const int WS_CHILD = 0x40000000;
+        const int WS_CLIPCHILDREN = 0x2000000;
+        IGraphBuilder GraphBuilder=null;//	 最为重用的COM接口,用于手动或者自动构造过滤通道Filter Graph Manager
+
+        IMediaControl MediaControl = null;//	用来控制流媒体，例如流的启动和停止暂停等,播放控制接口
+
+        IMediaSeeking MediaSeeking = null;//另一个播放的位置和播放速度控制接口,在位置选择方面功能较强.设置播放格式，多种控制播放方式.常用的有:(1)TIME_FORMAT_MEDIA_TIME单位100纳秒。(2)TIME_FORMAT_FRAME按帧播放
+
+        IMediaPosition MediaPosition = null;//播放的位置和速度控制接口(控制播放位置只能为设置时间控制方式)
+
+        IMediaEvent MediaEvent = null;//播放事件接口 ,该接口在Filter Graph发生一些事件时用来创建事件的标志信息并传送给应用程序
+
+        IMediaEventEx MediaEventEx = null;//扩展播放事件接口
+
+        IBasicAudio BasicAudio = null;//声音控制接口
+
+        IBasicVideo BasicVideo = null;//图像控制接口(波特率，宽度，长度等信息)
+
+        IVideoWindow VideoWindow = null;//显示窗口控制接口 (有关播放窗口的一切控制，包括caption显示，窗口位置控制等)
+
+        ISampleGrabber SampleGrabber = null;//捕获图象接口(可用于抓图控制)
+
+        IVideoFrameStep VideoFrameStep = null;//控制单帧播放的接口
+        //MediaSeeking.SetTimeFormat(TIME_FORMAT_FRAME);VideoFrameStep.Step(1,);
+        //VideoWindow.SetWindowPosition(0, 0, 100, 120);
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+        }
+        [DllImport("user32.dll")]//取设备场景 
+        private static extern IntPtr GetDC(IntPtr hwnd);//返回设备场景句柄 
+        [DllImport("gdi32.dll")]//取指定点颜色 
+        private static extern int GetPixel(IntPtr hdc, Point p);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+           
+            TIME_FORMAT_FRAME.ToString();
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "选择播放文件|*.mpg;*.avi;*.mov;*.wma;*.wav;*.mp3|All Files|*.*";
+            if (DialogResult.OK == openFile.ShowDialog())
+            {
+                //System.IO.Path.GetFileName(openFile.FileName);//得到文件名
+                //System.IO.Path.GetDirectoryName(openFile.FileName);//得到路径
+                file = System.IO.Path.GetDirectoryName(openFile.FileName) +"\\"+ System.IO.Path.GetFileName(openFile.FileName);
+                label17.Text =" "+ file;
+
+
+            }
+            open();
+            close();
+            open();
+         
+        }
+        private void open()
+        {
+            int hr;
+            if (this.GraphBuilder == null)
+            {
+                this.GraphBuilder = (IGraphBuilder)new FilterGraph();
+
+                hr = GraphBuilder.RenderFile(file, null);//读取文件
+                DsError.ThrowExceptionForHR(hr);
+                this.MediaControl = (IMediaControl)this.GraphBuilder;
+                this.MediaEventEx = (IMediaEventEx)this.GraphBuilder;
+                MediaSeeking = (IMediaSeeking)this.GraphBuilder;
+                MediaSeeking.SetTimeFormat(TIME_FORMAT_FRAME);
+                MediaSeeking.SetRate(0.3);
+                this.VideoFrameStep = (IVideoFrameStep)this.GraphBuilder;
+                // MediaPosition= (IMediaPosition)this.GraphBuilder;
+                this.VideoWindow = this.GraphBuilder as IVideoWindow;
+                this.BasicVideo = this.GraphBuilder as IBasicVideo;
+                this.BasicAudio = this.GraphBuilder as IBasicAudio;
+
+                hr = this.MediaEventEx.SetNotifyWindow(this.Handle, WM_GRAPHNOTIFY, IntPtr.Zero);
+                DsError.ThrowExceptionForHR(hr);
+
+
+                hr = this.VideoWindow.put_Owner(this.Handle);
+                DsError.ThrowExceptionForHR(hr);
+                hr = this.VideoWindow.put_WindowStyle(WindowStyle.Child |
+                    WindowStyle.ClipSiblings | WindowStyle.ClipChildren);
+                DsError.ThrowExceptionForHR(hr);
+                this.Focus();
+                hr = InitVideoWindow(1, 1);
+                DsError.ThrowExceptionForHR(hr);
+                long time;
+                MediaSeeking.GetDuration(out time);
+                label20.Text = time.ToString();
+                trackBar1.SetRange(0, (int)time);
+                t = new Thread(new ThreadStart(updateTimeBarThread));
+
+            }
+        }
+        private int InitVideoWindow(int nMultiplier, int nDivider)
+        {
+            int hr = 0;
+            int Height, Width;
+            if (this.BasicVideo == null)
+                return 0;
+            hr = this.BasicVideo.GetVideoSize(out Width, out Height);
+            if (hr == DsResults.E_NoInterface)
+            {
+                return 0;
+            }
+            //Width = Width * nMultiplier / nDivider;
+            //Height = Height * nMultiplier / nDivider;
+        
+            Application.DoEvents();
+            if (Width <= 500 && Height <= 500)
+            {
+                hr = this.VideoWindow.SetWindowPosition(0, 0, Width , Height);
+                label18.Text = (Width ).ToString();
+                label19.Text = (Height ).ToString();
+            }
+            else
+            {
+                hr = this.VideoWindow.SetWindowPosition(0, 0, Width / 2, Height / 2);
+                label18.Text =(Width / 2).ToString();
+                label19.Text =(Height / 2).ToString();
+            }
+            
+            return hr;
+        }
+        private void updateTimeBarThread()
+        {
+            long time;
+            int volu;
+            while(true)
+            {
+                try {
+                    MediaSeeking.GetCurrentPosition(out time);
+
+                    BasicAudio.get_Volume(out volu);
+
+                    this.BeginInvoke(new MethodInvoker(() => { trackBar1.Value = (int)time; }));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                
+                Thread.Sleep(1000);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+               
+
+          var b  = MediaControl.Run();
+          
+
+           
+            timer1.Enabled = true;
+           
+
+
+
+        }
+
+     
+
+        private void closeVideo()
+        {
+            int hr = 0;
+            if(this.MediaControl!=null)
+            {
+                hr = this.MediaControl.Stop();
+                closeInterfaces();
+
+            }
+
+        }
+        private void closeInterfaces()
+        {
+            int hr = 0;
+            try
+            {
+                lock (this)
+                {
+                    //relinquish ownership (IMPORTANT!) after hiding video window
+                    hr = this.VideoWindow.put_Visible(OABool.False);
+                    DsError.ThrowExceptionForHR(hr);
+                    hr = this.VideoWindow.put_Owner(IntPtr.Zero);
+                    DsError.ThrowExceptionForHR(hr);
+
+                    if (this.MediaEventEx != null)
+                    {
+                        hr = this.MediaEventEx.SetNotifyWindow(IntPtr.Zero, 0, IntPtr.Zero);
+                        DsError.ThrowExceptionForHR(hr);
+                    }
+                    //release and zero DirectShow interfaces
+                    if (this.MediaEventEx != null) this.MediaEventEx = null;
+                    if (this.MediaSeeking != null) this.MediaSeeking = null;
+                    if (this.MediaPosition != null) this.MediaPosition = null;
+                    if (this.MediaControl != null) this.MediaControl = null;
+                    if (this.BasicAudio != null) this.BasicAudio = null;
+                    if (this.BasicVideo != null) this.BasicVideo = null;
+                    if (this.VideoWindow != null) this.VideoWindow = null;
+                    if (this.VideoFrameStep != null) this.VideoFrameStep = null;
+                    if (this.GraphBuilder != null)
+                    {
+                       
+                        this.GraphBuilder = null;
+                    }
+                    GC.Collect();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            close();
+
+        }
+        private void close()
+        {
+            long time = 0;
+           
+            MediaSeeking.GetCurrentPosition(out time);
+            closeVideo();
+            t.Abort();
+            yy = 0;
+            //textBox1.Text = "";
+            trackBar1.Value = 0;
+           
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            long time = (long)trackBar1.Value;
+
+       
+            MediaSeeking.GetCurrentPosition(out time);
+            
+        }
+        static long yy;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (MediaSeeking != null)
+            {
+                long time=0;
+                MediaSeeking.GetCurrentPosition(out time);
+                label11.Text = time.ToString();
+                if(time-yy==1||time==trackBar1.Maximum)
+                {
+                    yy = time;
+                    timer1.Enabled = false;
+                    MediaControl.Pause();
+                   // textBox1.Text += trackBar1.Value+" ";
+                }
+                trackBar1.Value = (int)time;
+              
+            }
+        }
+
+    
+
+      
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Point screenPoint = Control.MousePosition;//鼠标相对于屏幕左上角的坐标
+            Point formPoint = this.PointToClient(Control.MousePosition);//鼠标相对于窗体左上角的坐标
+           label12.Text = formPoint.X.ToString();
+            label13.Text = formPoint.Y.ToString();
+            //label12.Text = Control.MousePosition.X.ToString();
+            // label13.Text = Control.MousePosition.Y.ToString();
+            Point p = new Point(MousePosition.X, MousePosition.Y);//取置顶点坐标 
+            IntPtr hdc = GetDC(new IntPtr(0));//取到设备场景(0就是全屏的设备场景)
+            int c = GetPixel(hdc, p);//取指定点颜色
+            label14.Text = (c & 0xFF).ToString();//转换R
+            label15.Text = ((c & 0xFF00) / 256).ToString();//转换G
+            label16.Text = ((c & 0xFF0000) / 65536).ToString();//转换B
+
+           
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.timer2.Enabled = true;
+            this.timer1.Interval = 10;//timer控件的执行频率
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process m_Process = null;
+            m_Process = new Process();
+            m_Process.StartInfo.FileName = "";
+            m_Process.Start();
+        }
+    }
+}
